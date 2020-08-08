4430465e04ef91621bff33802564ac19ba1754bd
Sergii Kamenskyi <serg@apple.com>
2020-08-08 10:19:24 +0300

    08.08.2020

---
+++ Problems (25)/Double greeting/task.py
@@ -0,0 +1,7 @@
+# the following lines read names from the input, do not modify it, please
+name_1 = input()
+name_2 = input()
+
+# your code here
+greeting(second_name=name_2, first_name=name_1)
+greeting(second_name=name_1, first_name=name_2)
